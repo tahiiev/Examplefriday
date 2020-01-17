886fa152293795de6192095f3cd7e8b978e425c0
tahiiev <tahiiev@sheridancollege.ca>
2020-01-17 16:22:40 -0500

    for friday

---
+++ src/examplefriday/ExampleFriday.java
@@ -0,0 +1,16 @@
+package examplefriday;
+
+/**
+ *
+ * @author avsim
+ */
+public class ExampleFriday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("learing GitHub");
+    }
+    
+}
